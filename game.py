d9360946bfbd5e9815feb781df5356c9e0ac7e5c
klj9236 <87046019+klj9236@users.noreply.github.com>
2021-07-12 22:01:34 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,3 +6,12 @@ print("Rock, Paper, Scissors, Shoot!")
 # ask for a user input
 x = input("Please choose either 'rock, 'paper', 'scissors'")
 print(x)
+
+# validate the user input
+
+if (x == "rock") or (x == "paper") or (x == "scissors"):
+    print("valid")
+else: 
+    print("Oops, invalid, please try again")
+exit()
+print("later messages")
